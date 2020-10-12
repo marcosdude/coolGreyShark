a9d5ffd95acf00027b39bf685f7c91879a4ae7c0
Marcos Manzanares <marcos.manzanares@patelhs.org>
2020-10-12 14:44:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main 
+{
+
+  public static void main(String[] args) 
+  {
+  String sharkName = "Advan";
+  int sharkWidth = 4;
+  int sharkHeight = 4;
+  int sharkLength = 15;
+  String color = "grey";
+  String sharkDiet = "vegan";
+  
+ 
+  }
+
+}
