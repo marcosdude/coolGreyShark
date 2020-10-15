47b66230d65cab7423a7affdf51a3f587c0965ff
Marcos Manzanares <marcos.manzanares@patelhs.org>
2020-10-15 23:28:23 +0000

    adding comments

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
 
 public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);//delacring an object
 
     System.out.println("What is the current wind speed in MPH?");
-    int wind = scan.nextInt();
+    int wind = scan.nextInt(); // asking user for an MPH
 
     String sharkName = "Advan";
     int sharkWidth = 4;
@@ -16,8 +16,10 @@ public static void main(String[] args){
     String sharkDiet = "vegan";
     String sharkPersonality = "cool";
     boolean sharkSeesFish = true;
+    boolean weatherConditionsAreGood = true;
+    // attributes, shark characteristics, and other variables
 
-    if (wind < 7) {
+    if (wind < 7 && weatherConditionsAreGood) {
       System.out.println("Advan will swim 10 feet underwater because the surface won't be choppy.");
     } else if (wind > 7) {
       System.out.println("Advan will swim 30 feet underwater because the surface will be choppy.");
