68202b9bc51ec266135239ce679c81b313f933fa
Marcos Manzanares <marcos.manzanares@patelhs.org>
2020-10-15 23:04:03 +0000

    added variable sharkPersonality.

--- Main.java
+++ Main.java
@@ -14,13 +14,14 @@ public static void main(String[] args){
     int sharkLength = 15;
     String color = "grey";
     String sharkDiet = "vegan";
+    String sharkPersonality = "cool";
     boolean sharkSeesFish = true;
 
 
     if (wind < 7) {
-      System.out.println("Advan will swim 10 feet underwater because the surface won't be 'choppy'");
+      System.out.println("Advan will swim 10 feet underwater because the surface won't be 'choppy'.");
     } else if (wind > 7) {
-      System.out.println("Advan will swim 30 feet underwater because the surface will be 'choppy'");
+      System.out.println("Advan will swim 30 feet underwater because the surface will be 'choppy'.");
     }
   }
 }
